aebb274137f17479451319c977c6b472fbe4a4b4
John Kousherian <hovig.kousherian@gmail.com>
2024-11-25 23:52:11 -0500

    S007: Added Documentation to AdaptiveSliders modifier extensions

--- Sources/AdaptiveSlider/Utilities/Extensions/PackageExtentions/AdaptiveSlider+Ext.swift
+++ Sources/AdaptiveSlider/Utilities/Extensions/PackageExtentions/AdaptiveSlider+Ext.swift
@@ -1,6 +1,26 @@
 import SwiftUI
 
 public extension AdaptiveSlider {
+	/// Sets the color or gradient used for the progress line.
+	///
+	/// - Parameter tint: An optional `AdaptiveStyle` (`Any ShapeStyle`) used to set the progress color or gradient.
+	///   If `nil`, the original color remains unchanged.
+	/// - Returns: An updated instance of `AdaptiveSlider` with the specified progress color.
+	///
+	/// Example with regular color:
+	/// ```swift
+	/// CircularSlider(value: $sliderValue, in: 0...100)
+	///     .tint(.red)
+	/// ```
+	///
+	/// Example with gradients:
+	/// ```swift
+	/// CircularSlider(value: $sliderValue, in: 0...100)
+	///     .tint(LinearGradient(
+	///				colors: [.red, .orange, .yellow],
+	///				startPoint: .top,
+	///				endPoint: .bottom))
+	/// ```
 	func tint(_ tint: AdaptiveStyle?) -> Self {
 		guard let tint else { return self }
 		var copy = self
@@ -10,6 +30,10 @@ public extension AdaptiveSlider {
 }
 
 extension AdaptiveSlider {
+	/// Provides a computed property for the progress line's fill.
+	///
+	/// If a custom progress color (`progressColor`) is set, it returns that color.
+	/// Otherwise, it defaults to system blue color (`Color(.systemBlue)`).
 	var progressFill: AnyShapeStyle {
 		if let progressColor {
 			return AnyShapeStyle(progressColor)
@@ -20,6 +44,18 @@ extension AdaptiveSlider {
 }
 
 public extension AdaptiveSlider {
+	/// Sets the style of the track, including its width and color.
+	///
+	/// - Parameters:
+	///   - lineWidth: A `CGFloat` specifying the width of the track line.
+	///   - color: An optional `Color` to set as the track color. Defaults to `.systemGray5` on iOS or `.gray` on other platforms.
+	/// - Returns: An updated instance of `AdaptiveSlider` with the specified track style.
+	///
+	/// Example usage:
+	/// ```swift
+	/// CircularSlider(value: $sliderValue, in: 0...100)
+	///     .trackStyle(lineWidth: 5, color: .gray)
+	/// ```
 	func trackStyle(lineWidth: CGFloat, color: Color? = nil) -> Self {
 		var copy = self
 		copy.lineWidth = lineWidth
@@ -33,6 +69,18 @@ public extension AdaptiveSlider {
 }
 
 public extension AdaptiveSlider {
+	/// Customizes the thumb's radius and color.
+	///
+	/// - Parameters:
+	///   - radius: A `CGFloat` specifying the radius of the thumb.
+	///   - color: A `Color` specifying the thumb color. Default is `.white`.
+	/// - Returns: An updated instance of `AdaptiveSlider` with the specified thumb style.
+	///
+	/// Example usage:
+	/// ```swift
+	/// CircularSlider(value: $sliderValue, in: 0...100)
+	///     .thumbStyle(radius: 10, color: .red)
+	/// ```
 	func thumbStyle(radius: CGFloat, color: Color = .white) -> Self {
 		var copy = self
 		copy.thumbRadius = radius
@@ -42,6 +90,19 @@ public extension AdaptiveSlider {
 }
 
 public extension AdaptiveSlider {
+	/// Adds visual markers (ticks) to the slider to enhance user interaction.
+	///
+	/// - Parameters:
+	///   - count: An `Int` representing the number of ticks to be shown.
+	///   - size: An optional `CGSize` specifying the size of each tick. Defaults to `CGSize(width: 1.5, height: lineWidth)`.
+	///   - color: An optional `Color` specifying the tick color. Defaults to `.systemGray2` on iOS or `.gray` on other platforms.
+	/// - Returns: An updated instance of `AdaptiveSlider` with the specified ticks.
+	///
+	/// Example usage:
+	/// ```swift
+	/// CircularSlider(value: $sliderValue, in: 0...100)
+	///     .showTicks(count: 10, size: CGSize(width: 2, height: 5), color: .black)
+	/// ```
 	func showTicks(
 		count: Int,
 		size: CGSize? = nil,
@@ -62,6 +123,16 @@ public extension AdaptiveSlider {
 
 #if os(iOS)
 public extension AdaptiveSlider {
+	/// Adds haptic feedback to the slider during interaction.
+	///
+	/// - Parameter style: A `UIImpactFeedbackGenerator.FeedbackStyle` specifying the type of haptic feedback.
+	/// - Returns: An updated instance of `AdaptiveSlider` with the specified haptic feedback.
+	///
+	/// Example usage:
+	/// ```swift
+	/// CircularSlider(value: $sliderValue, in: 0...100)
+	///     .hapticFeedback(.medium)
+	/// ```
 	func hapticFeedback(_ style: UIImpactFeedbackGenerator.FeedbackStyle) -> Self {
 		var copy = self
 		copy.feedbackGenerator = UIImpactFeedbackGenerator(style: style)
@@ -73,7 +144,7 @@ public extension AdaptiveSlider {
 public extension AdaptiveSlider {
 	/// Enables customization of the accessibility **Value**, **Hint**, and **Label** for `AdaptiveSlider`.
 	///
-	/// - Note: By default the accessibilityValue is set to whatever is the current value of the slider is suffixed by "Precent", both accessibilityHint and accessibilityLabel are empty strings.
+	/// - Note: By default, the `accessibilityValue` is set to whatever the current value of the slider is, suffixed by "Percent". Both `accessibilityHint` and `accessibilityLabel` are empty strings.
 	///
 	/// - Parameters:
 	///   - value: A `String` representing the current value of the slider (e.g., "75 percent").
@@ -86,6 +157,12 @@ public extension AdaptiveSlider {
 	///            such as "Volume control".
 	///
 	/// - Returns: A modified instance of `AdaptiveSlider` with the specified accessibility properties.
+	///
+	/// Example usage:
+	/// ```swift
+	/// CircularSlider(value: $sliderValue, in: 0...100)
+	///     .accessibilityValue("75 percent", hint: "Adjust the temperature setting", label: "Temperature Control")
+	/// ```
 	func accessibilityValue(_ value: String, hint: String = "", label: String = "") -> Self {
 		var copy = self
 		copy.accessibilityValue = value
